27ce9a5e5b9f3413b368e993d6454fc381dc8940
Tejas Sarma <sarmatejas1006@gmail.com>
2017-12-01 16:04:14 -0600

    Added TutorLocationActivity. Extracted address from pointed location

--- app/src/main/java/com/hci/project/cerebro/TutoringLocationActivity.java
+++ app/src/main/java/com/hci/project/cerebro/TutoringLocationActivity.java
@@ -43,15 +43,7 @@ public class TutoringLocationActivity extends FragmentActivity implements OnMapR
 
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    //GET ADDRESS from location latitude, longitude
     private String getAddress(double latitude, double longitude) {
         StringBuilder result = new StringBuilder();
         try {
@@ -78,13 +70,6 @@ public class TutoringLocationActivity extends FragmentActivity implements OnMapR
         // Add a marker in Sydney and move the camera
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         mFusedLocationClient.getLastLocation()
@@ -101,6 +86,7 @@ public class TutoringLocationActivity extends FragmentActivity implements OnMapR
                         }
                     }
                 });
+
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
