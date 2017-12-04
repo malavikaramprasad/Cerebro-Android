e26707d3a1ad00c6291fad9c279de4ee5c649f3e
Malavika Ramprasad <malavika.ramprasad@gmail.com>
2017-12-03 18:32:56 -0600

    removed settings option

--- app/src/main/java/com/hci/project/cerebro/DrawerActivity.java
+++ app/src/main/java/com/hci/project/cerebro/DrawerActivity.java
@@ -215,9 +215,9 @@ public class DrawerActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
